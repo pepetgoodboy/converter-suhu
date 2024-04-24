a6f3912494fddc21fde4aae058fb2b5a67f67c50
pepetgoodboy <iqbaltw127@gmail.com>
2024-04-24 21:52:35 +0700

    styling css

--- js/script.js
+++ js/script.js
@@ -1,4 +1,4 @@
-function konversi() {
+function konversiCtoF() {
   var celsius = document.getElementById("celsius").value;
   var fahrenheit = (celsius * 9) / 5 + 32;
   let deg = "°";
@@ -12,23 +12,40 @@ function konversi() {
   }
 }
 
+function konversiFtoC() {
+  var fahrenheit = document.getElementById("fahrenheitt").value;
+  var celsius = ((fahrenheit - 32) * 5) / 9;
+  let deg = "°";
+  if (fahrenheit == "") {
+    alert("Masukkan Angka!");
+    reset();
+  } else {
+    document.getElementById("celsiuss").innerText = celsius;
+    document.getElementById("formulas").innerText =
+      fahrenheit + deg + "F" + " - 32 * 5/9 = " + celsius + deg + "C";
+  }
+}
+
 function reset() {
   document.getElementById("celsius").value = "";
   document.getElementById("fahrenheit").innerText = "";
   document.getElementById("formula").innerText = "";
+  document.getElementById("celsiuss").innerText = "";
+  document.getElementById("fahrenheitt").value = "";
+  document.getElementById("formulas").innerText = "";
 }
 
+let isReverse = true;
 function reverse() {
-  var celsius = document.getElementById("celsius").value;
-  var fahrenheit = (celsius * 9) / 5 + 32;
-  var reversed = ((fahrenheit - 32) * 5) / 9;
-  let deg = "°";
-  if (celsius == "") {
-    alert("Masukkan Angka!");
-    reset();
+  let cToF = document.getElementById("c-to-f");
+  let fToC = document.getElementById("f-to-c");
+  if (isReverse) {
+    fToC.style.display = "block";
+    cToF.style.display = "none";
+    isReverse = false;
   } else {
-    document.getElementById("celsius").value = reversed;
-    document.getElementById("formula").innerText =
-      fahrenheit + deg + "F " + "- " + 32 + " * 5/9 = " + celsius + deg + "C";
+    cToF.style.display = "block";
+    fToC.style.display = "none";
+    isReverse = true;
   }
 }
